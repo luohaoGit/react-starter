bc583a8830b59f96c46efcf934e37e91e8975ca0
luohao <553569682@qq.com>
2015-08-21 15:09:34 +0800

    hash jump

--- assets/components/table.js
+++ assets/components/table.js
@@ -4,51 +4,82 @@ let PureRenderMixin = React.addons.PureRenderMixin;
 var Table = React.createClass({
 	mixins: [PureRenderMixin],
 
+	_getHead(type){
+		switch(type)
+		{
+			case 0:
+				break;
+			case 1:
+
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+		}
+	},
+
 	propTypes: {
-		title: React.PropTypes.string.isRequired
+		title: React.PropTypes.string,
+		page: React.PropTypes.Boolean,
+		pageBarSize: React.PropTypes.number,
+		type: React.PropTypes.number
 	},
 
 	getDefaultProps() {
 		return {
-			title: '表格'
+			title: '表格',
+			page: true,
+			pageBarSize: 5,
+			type: 0
 		};
 	},
 
 	render() {
-		var {className, title, header, colums, rows, data, ...other} = this.props;
+		var {className, title, type,
+			footSpan, page, pageBarSize, data, ...other} = this.props;
+
+		let head = this._getHead(type);
+
+		let pageBar = [];
+		for (let i = 0; i < pageBarSize; i++) {
+			pageBar.push(
+				<a className="item">{i}</a>
+			);
+		}
+
 		return (
-			<table className="ui celled structured table">
-				<thead>
-				<tr><th colSpan="11">{title}</th></tr>
-				<tr>
-					<th rowSpan="2">学校</th>
-					<th rowSpan="2">平均分</th>
-					<th rowSpan="2">最高分</th>
-					<th rowSpan="2">最低分</th>
-					<th rowSpan="2">优秀率</th>
-					<th rowSpan="2">及格率</th>
-					<th colSpan="5">档次</th>
-				</tr>
-				<tr>
-					<th>A 90-100分</th>
-					<th>B 80-89分</th>
-					<th>C 70-19分</th>
-					<th>D 60-69分</th>
-					<th>E 59分以下</th>
-				</tr>
-				</thead>
-				<tbody>
-				<tr>
-					<td>第一小学</td>
-					<td>83</td>
-					<td>100</td>
-					<td>53</td>
-					<td>87%</td>
-					<td>95%</td>
-					<td>569</td><td>597</td><td>377</td><td>270</td><td>54</td>
-				</tr>
-				</tbody>
-			</table>
+			<div>
+				<h1 className="ui header">title</h1>
+				<table className="ui celled structured table">
+					<thead>
+					{head}
+					</thead>
+
+					<tbody>
+
+					</tbody>
+
+					{page && (
+						<tfoot>
+						<tr>
+							<th colSpan={footSpan}>
+								<div className="ui right floated pagination menu">
+									<a className="icon item">上一页</a>
+									{pageBar}
+									<a className="icon item">写一页</a>
+								</div>
+							</th>
+						</tr>
+						</tfoot>
+					)}
+				</table>
+			</div>
 		);
 	}
 });
--- assets/tAreaSchool.js
+++ assets/tAreaSchool.js
@@ -20,7 +20,7 @@ var TAreaSchool = React.createClass({
                 <Container>
                     <HighChart style={{height: 400 + 'px'}} type="column" title="同一知识点所有学校对比统计" yTitle="得分率（%）" seriesName="得分率"
                                legend={true} unit="%" colorByPoint={false} dataLabels={true}
-                               xCategories={["利用商不变的规律进行简便计算"]} clickEvent={true} link="#/tSchoolClazz"
+                               xCategories={["利用商不变的规律进行简便计算", "知识点1", "知识点2", "知识点3"]} clickEvent={true} link="#/tSchoolClazz"
                                data={this.state.get('chart12')}
                         />
                 </Container>
